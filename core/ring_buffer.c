4185b78698fb359c24387f28ff73e582467970a3
Yashaswini-200 <171708816+Yashaswini-200@users.noreply.github.com>
2026-05-20 14:26:36 +0000

    push and pop

--- core/ring_buffer.c
+++ core/ring_buffer.c
@@ -1,7 +1,35 @@
+#include<stdint.h>
+#include<stdio.h>
+#include<stdbool.h>
 #define RING_BUFFER_SIZE 128U
+
 typedef struct{
     uint8_t buffer[RING_BUFFER_SIZE];
-    Uint8_t head;
-    uint8_t tail;
+    volatile uint8_t head;
+    volatile uint8_t tail;
 } RingBuffer;
 
+bool rb_empty(RingBuffer *rb){
+    return rb->head==rb->tail;
+}
+
+bool rb_full(RingBuffer *rb){
+    return ((rb->head+1)%RING_BUFFER_SIZE)==rb->tail;
+}
+bool rb_push(RingBuffer *rb , uint8_t data){
+    if(rb_full(rb)){
+        return false;
+    }
+
+    rb->buffer[rb->head]=data;
+    rb->head = (rb->head+1)%(RING_BUFFER_SIZE);
+    return true;
+}
+bool rb_pop(RingBuffer *rb , uint8_t *data){
+    if(rb_empty(rb)){
+        return false;
+    }
+    *data=rb->buffer[rb->tail];
+    rb->tail++;
+    return true;
+}
